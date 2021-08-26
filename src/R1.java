40ff8622f3ce998bc0e1294d2ae85afd12e2b60d
Gudinsun <Gudinsun@mail.ru>
2021-08-26 17:36:59 +0300

    Initial commit

---
+++ src/R1.java
@@ -0,0 +1,7 @@
+public class R1 {
+    public static void main(String[] args) {
+        int i;
+        for ( i = 0; i < 102; i++)
+            System.out.println(i);
+    }
+}
---
+++ src/R3.java
@@ -0,0 +1,13 @@
+public class R3 {
+    public static void main(String[] args) {
+        for (int i=0; i<=100;  i++){
+            if (i==90){
+                continue;
+            }
+            if (i%2==0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
